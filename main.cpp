9ffb4429e12ea24b9f04f13f893292aa3eac70d1
Harshit Goyal <97736991+Harshit0143@users.noreply.github.com>
2024-05-30 20:52:42 +0530

    started

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <iterator>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <bitset>
+#include <random>
+#include <map>
+#include <unordered_map>
+#include <stdio.h>
+#include <complex>
+#include <math.h>
+#include <cstring>
+#include <chrono>
+#include <string>
+
+using namespace std;
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define fo(i, a, b) for (int i = a; i < b; ++i)
+#define max_(a, b, c) max(max(a, b), c)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+class Bug{
+    Bug()
+};
+
+class Board{
+    Board(vector<vector<int>> &board)
+    {
+
+
+    }
+};
+
+int main()
+{
+    vector<string> bugs = {"snail", "spider", "grasshopper", "ladybug", "honeybee", "butterfly"};
+    fastio;
+    int t = 1;
+    cin >> t;
+    fo(i, 0, t)
+            cout
+        << f() << "\n";
+    return 0;
+}
